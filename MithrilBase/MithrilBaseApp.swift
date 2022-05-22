aefbb634a5a7cda2c4cd9365d06b99f5c01e850e
Kristian Mitra <xanril@users.noreply.github.com>
2022-05-22 12:43:17 +0800

    initial commit

---
+++ MithrilBase/MithrilBaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  MithrilBaseApp.swift
+//  MithrilBase
+//
+//  Created by Kristian Mitra on 5/22/22.
+//
+
+import SwiftUI
+
+@main
+struct MithrilBaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
